a7a14e887ca9248864a526d9feacb5730abb4e62
Reland <reland.boyle@gmail.com>
2025-08-30 20:30:28 -0400

    Scope adjustment

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: "openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents",
+          scope: "openid email profile https://www.googleapis.com/auth/drive.file",
         },
       },
     }),
